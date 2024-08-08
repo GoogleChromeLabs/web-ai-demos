64e50564e53ed0ce8bd9dfcf4daf47b1abcaec72
André Cipriani Bandarra <andreban@gmail.com>
2024-08-08 07:03:39 +0100

    Update summarization-api-playground/src/main.ts
    Co-authored-by: Thomas Steiner <tomac@google.com>

--- summarization-api-playground/src/main.ts
+++ summarization-api-playground/src/main.ts
@@ -6,7 +6,7 @@
 import './style.css'
 
 // The underlying model has a context of 1,024 tokens, out of which 26 are used by the internal prompt,
-// leaving about 998 tokens for the input text. Each token is, roughly, about 4 characters, so 4000
+// leaving about 998 tokens for the input text. Each token corresponds, roughly, to about 4 characters, so 4,000
 // is used as a limit to warn the user the content might be too long to summarize.
 const MAX_MODEL_CHARS = 4000;
 const inputTextArea = document.querySelector('#input') as HTMLTextAreaElement;
