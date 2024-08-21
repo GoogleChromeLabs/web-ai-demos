f35bd84b42546bc06758a974c160f5d4fe860899
maudnals <maud.nalpas@gmail.com>
2024-08-21 16:33:54 +0200

    Feat: Unified review output status with model status

--- helpful-product-reviews/src/script.js
+++ helpful-product-reviews/src/script.js
@@ -1,9 +1,13 @@
 const url = new URL('./worker.js', import.meta.url);
 import { MESSAGE_CODE, MODEL_STATUS } from './consts.js';
 
+// Initialization
+
 displayModelStatus(MODEL_STATUS.NOT_STARTED);
 const worker = new Worker(url);
 let typingTimeout = 0;
+
+// DOM manipulation and events
 
 const handleUserInputChange = (event) => {
   if (typingTimeout) {
@@ -15,19 +19,6 @@ const handleUserInputChange = (event) => {
   }, 800); // 800ms delay
 };
 
-// onchange="alert('the textarea has change')"
-// handleInput(
-//   'Review: "Love the natural ingredients!" Helpful: yes Suggestion: none'
-// );
-
-// handleInput(
-//   'Review: "I don\'t like the color." Helpful: no Suggestion: Be more specific. For example, "The color is too dark for my hair."'
-// );
-
-// handleInput(
-//   'Review: "Love the natural ingredients!" Helpful: yes Suggestion: none'
-// );
-
 function displayModelStatus(status) {
   document.getElementById('modelStatusWrapper').className = status;
   document.getElementById('modelStatus').className = status;
@@ -37,14 +28,16 @@ function displayReviewHelperOutput(output) {
   document.getElementById('reviewHelperOutput').innerText = output;
 }
 
-function runLLMInference() {
-  const userPrompt = document.getElementById('reviewInputEl').value;
-  worker.postMessage(userPrompt);
-}
-
 function simulatePostReview() {
   document.getElementById('reviewInputEl').value = '';
   window.alert('Review posted!');
+}
+
+// Gen AI / inference + worker message handling
+
+function runLLMInference() {
+  const userPrompt = document.getElementById('reviewInputEl').value;
+  worker.postMessage(userPrompt);
 }
 
 worker.onmessage = function (message) {
@@ -59,27 +52,33 @@ worker.onmessage = function (message) {
   switch (messageCode) {
     case MESSAGE_CODE.PREPARING_MODEL:
       displayModelStatus(MODEL_STATUS.PREPARING);
-      document.getElementById('reviewHelperStatus').className = 'not-ready';
+      document.getElementById('reviewHelperStatus').className =
+        MODEL_STATUS.PREPARING;
       break;
 
     case MESSAGE_CODE.MODEL_READY:
-      document.getElementById('reviewHelperStatus').className = 'ready';
       displayModelStatus(MODEL_STATUS.READY);
+      document.getElementById('reviewHelperStatus').className =
+        MODEL_STATUS.READY;
       break;
 
     case MESSAGE_CODE.GENERATING_RESPONSE:
       displayModelStatus(MODEL_STATUS.GENERATING);
+      document.getElementById('reviewHelperStatus').className =
+        MODEL_STATUS.GENERATING;
       break;
 
     case MESSAGE_CODE.RESPONSE_READY:
       displayModelStatus(MODEL_STATUS.READY);
+      document.getElementById('reviewHelperStatus').className =
+        MODEL_STATUS.READY;
       displayReviewHelperOutput(message.data.payload);
       // document.getElementById('llmOutput').innerText = message.data.payload;
       break;
 
     case MESSAGE_CODE.MODEL_ERROR:
-      document.getElementById('reviewHelperStatus').className = 'error';
       displayModelStatus(MODEL_STATUS.ERROR);
+      document.getElementById('reviewHelperStatus').className = 'error';
       break;
 
     default:
