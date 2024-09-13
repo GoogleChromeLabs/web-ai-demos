bacd9c50c05630d716757164ed572df5f1b81ac1
maudnals <maud.nalpas@gmail.com>
2024-09-13 13:33:29 +0200

    Chore: Added OSS headers

--- toxic-review-warning/src/config.js
+++ toxic-review-warning/src/config.js
@@ -1,3 +1,6 @@
+/* Copyright 2024 Google LLC
+SPDX-License-Identifier: Apache-2.0 */
+
 // How long to wait after the user stops typing until we trigger the AI inference
 export const TYPING_DELAY = 800;
 
