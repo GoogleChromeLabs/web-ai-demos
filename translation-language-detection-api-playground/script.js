605b51183356e5e0767485a89be519bb637de7f7
Thomas Steiner <tomac@google.com>
2024-09-19 10:12:10 +0200

    Add license headers

--- translation-language-detection-api-playground/script.js
+++ translation-language-detection-api-playground/script.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 (async () => {
   if (!('translation' in self)) {
     document.querySelector('.not-supported-message').hidden = false;
