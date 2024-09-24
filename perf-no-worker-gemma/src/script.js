bbe74189da1fa167249b93e8681445e907a184cb
maudnals <maud.nalpas@gmail.com>
2024-09-24 17:30:22 +0200

    Addressed review comments + Renamed folders

--- perf-no-worker-gemma/src/script.js
+++ perf-no-worker-gemma/src/script.js
@@ -1,22 +1,23 @@
-import { MESSAGE_CODE, MODEL_STATUS } from './consts.js';
+import { MODEL_STATUS } from './consts.js';
 import { FilesetResolver, LlmInference } from '@mediapipe/tasks-genai';
 import { MODEL_URL, MEDIAPIPE_WASM, MESSAGE_CODE } from './consts.js';
 
 displayModelStatus(MODEL_STATUS.NOT_STARTED);
+const inferenceButton = document.getElementById('inferenceButton');
 let llmInference = null;
 
 (async function () {
   displayModelStatus(MODEL_STATUS.PREPARING);
-  document.getElementById('inferenceButton').disabled = true;
+  inferenceButton.setAttribute('aria-disabled', 'true');
   try {
     const genai = await FilesetResolver.forGenAiTasks(MEDIAPIPE_WASM);
     llmInference = await LlmInference.createFromModelPath(genai, MODEL_URL);
     displayModelStatus(MODEL_STATUS.READY);
-    document.getElementById('inferenceButton').disabled = false;
+    inferenceButton.setAttribute('aria-disabled', 'false');
   } catch (error) {
     console.error('[Worker] Error preparing model:', error);
     displayModelStatus(MODEL_STATUS.ERROR);
-    document.getElementById('inferenceButton').disabled = true;
+    inferenceButton.setAttribute('aria-disabled', 'true');
   }
 })();
 
@@ -31,13 +32,13 @@ function runLLMInference() {
     throw new Error("Can't run inference, the model is not ready yet");
   }
   displayModelStatus(MODEL_STATUS.GENERATING);
-  document.getElementById('inferenceButton').disabled = true;
+  inferenceButton.setAttribute('aria-disabled', 'true');
 
   (async function () {
     // TODO handle errors (e.g. an inference error can happen when the input is too long). A simple try/catch isn't sufficient, we also need to terminate the previous/failing inference which I didn't figure out how to do
     const response = await llmInference.generateResponse(userPrompt);
     displayModelStatus(MODEL_STATUS.READY);
-    document.getElementById('inferenceButton').disabled = false;
+    inferenceButton.setAttribute('aria-disabled', 'false');
     document.getElementById('llmOutput').innerText = response;
   })();
 }
