baf5ed07c142b9f3bd73a7881c7a20a63f660f6b
Mike Wasserman <michaelwasserman@users.noreply.github.com>
2025-12-11 13:41:41 -0800

    Fix canvas-image-prompt transparency

--- canvas-image-prompt/script.js
+++ canvas-image-prompt/script.js
@@ -37,6 +37,8 @@ canvas.height = referenceImage.height;
 let isPainting = false;
 
 const ctx = canvas.getContext("2d");
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.lineWidth = 4;
 ctx.lineCap = "round";
 
