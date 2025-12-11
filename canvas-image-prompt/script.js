62f92722876b73eb0f9f4210ff8ef4f48c30cb2d
Mike Wasserman <michaelwasserman@users.noreply.github.com>
2025-12-11 13:46:36 -0800

    fix style

--- canvas-image-prompt/script.js
+++ canvas-image-prompt/script.js
@@ -41,7 +41,7 @@ ctx.fillStyle = "white";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.lineWidth = 4;
 ctx.lineCap = "round";
-ctx.strokeStyle = "blue";
+ctx.strokeStyle = "black";
 
 
 const draw = ({ clientX, clientY }) => {
