a3cd7d5bf60b253841c5572d5fc7e35e68d569a8
Thomas Steiner <tomac@google.com>
2026-08-17 17:18:58 +0200

    Add missing license headers

--- wordup/e2e/runner.ts
+++ wordup/e2e/runner.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2026 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { mount, unmount, tick } from 'svelte';
 import App from '../src/App.svelte';
 import { IDBFactory } from 'fake-indexeddb';
--- wordup/svelte.config.js
+++ wordup/svelte.config.js
@@ -1,2 +1,7 @@
+/**
+ * Copyright 2026 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 /** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
 export default {}
